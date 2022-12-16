1d34bb6e425f15fac0396751bee7ef0c0b88c182
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2022-12-17 00:08:15 +0300

    added function lesson and hw

---
+++ pygame/1_introduction/solve.py
@@ -0,0 +1,25 @@
+def first():
+    a = int(input())
+    b = float(input())
+    c = bool(input())
+    d = str(a)
+    e = str(b)
+    f = str(c)
+    print(a, b, c)
+    print(d, e, f)
+
+
+def second():
+    a = int(input())
+    print(a)
+    print(a)
+
+
+def third():
+    a = float(input())
+    print(a)
+
+
+def fourth():
+    name = input()
+    print("Hello, " + name + "!")
