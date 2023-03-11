8b66935b8a6f95f41aa120a6370348cb70cddb25
Коворкинг 10 <marsel.ahmetov974@gmail.com>
2023-03-11 15:44:44 +0300

    fixed turn head

--- python/9_pygame_start/snake/head.py
+++ python/9_pygame_start/snake/head.py
@@ -12,7 +12,10 @@ class Head(pygame.sprite.Sprite):
     def set_image(self, file):
         self.image = pygame.image.load(file).convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.radius, self.radius))
-        self.rect = self.image.get_rect(center=self.position)
+        self.rect = self.image.get_rect(center=self.fixed_position())
+
+    def fixed_position(self):
+        return self.position[0] + self.radius // 2, self.position[1] + self.radius // 2
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -20,6 +23,7 @@ class Head(pygame.sprite.Sprite):
     def move(self, direction, speed):
         self.rect.x = (self.rect.x + direction[0] * speed) % 800
         self.rect.y = (self.rect.y + direction[1] * speed) % 600
+        self.position = (self.rect.x, self.rect.y)
 
     def right(self):
         self.set_image("head_right.png")
@@ -32,3 +36,6 @@ class Head(pygame.sprite.Sprite):
 
     def left(self):
         self.set_image("head_left.png")
+
+    def set_position(self, position):
+        self.position = position
